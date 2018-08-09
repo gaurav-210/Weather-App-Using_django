d275ec53b0bcf91cc58de4d96eb7f27b81e2b570
gaurav-210 <karan02555@gmail.com>
2018-08-09 17:32:54 +0530

    add new features

--- manage.py
+++ manage.py
@@ -2,8 +2,8 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_weather.settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
